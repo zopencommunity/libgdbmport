6958e4287c358bdbe764d20897c76ab99972a244
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-05-09 12:43:49 -0400

    Update cicd.groovy

--- cicd.groovy
+++ cicd.groovy
@@ -1,6 +1,6 @@
 node('linux')
 {
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gdbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU dbm is a set of database routines that use extendible hashing and works similar to the standard UNIX dbm routines.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgdbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU dbm is a set of database routines that use extendible hashing and works similar to the standard UNIX dbm routines.' )]
   }
 }
