77797f7b36b9df49a8bd1ca1f578dfbf6cc259ab
Igor Todorovski <itodorov@ca.ibm.com>
2023-05-24 13:29:06 -0400

    add polling to cicd.groovy script

--- cicd.groovy
+++ cicd.groovy
@@ -1,5 +1,14 @@
 node('linux')
 {
+   
+  stage (Poll) {
+                checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/main']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libgdbmport.git"]]])
+        }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgdbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU dbm is a set of database routines that use extendible hashing and works similar to the standard UNIX dbm routines.' )]
   }
