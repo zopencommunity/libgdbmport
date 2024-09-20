d675a2e05ba479c6e2d0606bff4a0ee4d19027c1
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:58 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,9 +7,9 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/libgdbmport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/libgdbmport.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/libgdbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU dbm is a set of database routines that use extendible hashing and works similar to the standard UNIX dbm routines.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/libgdbmport.git'), string(name: 'PORT_DESCRIPTION', value: 'GNU dbm is a set of database routines that use extendible hashing and works similar to the standard UNIX dbm routines.' )]
   }
 }
